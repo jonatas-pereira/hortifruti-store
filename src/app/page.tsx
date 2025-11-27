8c1e257855f9c5314f225bfe419445fe38e355a4
jonatas-pereira <jonataspereirarocha@gmail.com>
2025-11-27 12:45:39 -0300

    refactor: change file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import ProductsContainer from "@/components/products-container/products-container";
 import Container from "@/components/container/container";
 import Devider from "@/components/devider/devider";
+import BannerSection from "@/components/banner/banner-section";
 
 import { Products } from "@/mocks/products-mock"
+import { bannerImgArray } from "@/mocks/banner-img-array";
 
 export default function Home() {
   return (
     <div>
       <Container>
+        <BannerSection photos={bannerImgArray}/>
         <Devider msg="Hortaliças" />
         <ProductsContainer category="hortaliças" products={Products}/>
         <Devider msg="Vegetais" />
