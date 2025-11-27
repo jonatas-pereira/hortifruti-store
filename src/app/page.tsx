7c3ede64fdff991a214596bcbae603553afe4f89
jonatas-pereira <jonataspereirarocha@gmail.com>
2025-11-27 11:10:44 -0300

    refactor: modified files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import ProductsContainer from "@/components/products-container/products-container";
+import Container from "@/components/container/container";
+import Devider from "@/components/devider/devider";
+
+import { Products } from "@/mocks/products-mock"
+
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <Container>
+        <Devider msg="Hortaliças" />
+        <ProductsContainer category="hortaliças" products={Products}/>
+        <Devider msg="Vegetais" />
+        <ProductsContainer category="vegetais" products={Products}/>
+      </Container>
     </div>
   );
 }
