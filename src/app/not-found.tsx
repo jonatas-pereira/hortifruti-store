40306de272505e08e79b07e5b960a4841c965a10
jonatas-pereira <jonataspereirarocha@gmail.com>
2025-11-29 00:52:03 -0300

    feat: created not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1>Página não encontrada</h1>  
+      <div>
+        <a href="/">
+          <button>Voltar para a página inicial</button>
+        </a>
+      </div>
+    </div>
+  )
+}
