4e92cba9ab972097f0b545c6c79d7bf99280f3ab
jonatas-pereira <jonataspereirarocha@gmail.com>
2025-11-29 01:50:47 -0300

    refactor: modified file

--- src/app/api/products/[id]/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { supabase } from '@/db/supabase'
-
-export async function GET(request: NextRequest) {
-  return NextResponse.json({ message: `` });
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,18 @@ import Container from "@/components/container/container";
 import Devider from "@/components/devider/devider";
 import BannerSection from "@/components/banner/banner-section";
 import Product from "@/types/product";
-import { bannerImgArray } from "@/mocks/banner-img-array";
 
 export default async function Home() {
 
-  const firstSectionReq = await fetch(`${process.env.DOMAIN || "http://localhost:3000"}/api/products?category=hortaliça`)
+  const firstSectionReq = await fetch(`${process.env.DOMAIN}/api/products?category=hortaliça`)
   const firstSectionData: Product[] = await firstSectionReq.json().then(res => res.data)
 
-  const secondSectionReq = await fetch(`${process.env.DOMAIN || "http://localhost:3000"}/api/products?category=vegetal`)
+  const secondSectionReq = await fetch(`${process.env.DOMAIN}/api/products?category=vegetal`)
   const secondSectionData: Product[] = await secondSectionReq.json().then(res => res.data)
 
   return (
     <div>
       <Container>
-        <BannerSection photos={bannerImgArray}/>
         { firstSectionData.length > 0 && <Devider msg="Hortaliças" /> } 
         { firstSectionData.length > 0 && <ProductsContainer category="hortaliças" products={firstSectionData}/> }
         { secondSectionData.length > 0 && <Devider msg="Vegetais" />}
