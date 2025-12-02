2d263a5cb05cf31270366f32c404014a66ba122c
jonatas-pereira <jonataspereirarocha@gmail.com>
2025-12-01 23:12:43 -0300

    feat: created footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,9 +1,27 @@
-"use client"
+import styles from "./footer.module.scss"
+
+import Container from "../container/container"
+import Logo from "@/assets/logo.png"
+import Image from "next/image"
+import { FaHeart } from "react-icons/fa"
+
 
 export default function Footer() {
   return (
-    <footer>
-      <h1>FOOTER</h1>
+    <footer className={styles.footer}>
+      <Container>
+        <div className={styles.footerWrapper}>
+          <div className={styles.brandInfo}>
+            <Image src={Logo} alt="Logo do Hortifruti São Felipe" width={90} />
+            &copy; 2025 Hortifruti São Felipe. Todos os direitos reservados.
+          </div>
+          <div className={styles.techInfo}>
+            <span>
+              Desenvolvido com <FaHeart color="red" width={30}/> por Equipe Hortifruti São Felipe.
+            </span>
+          </div>
+        </div>
+      </Container>
     </footer>
   )
 }
